d5eb6e9d3b523d5658a882e48f87428ee87c01bc
Jack McNicol <jackmcpickle@gmail.com>
2021-01-21 21:51:15 +1030

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    env: {
+        es6: true,
+        browser: true,
+        commonjs: true,
+        node: true,
+        jest: true,
+    },
+    globals: {
+        $: true,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    plugins: ['prettier', 'jsx-a11y'],
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'react-app',
+        'react-app/jest',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+    ],
+    rules: {
+        'no-alert': 0,
+        'no-console': 0,
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+};
